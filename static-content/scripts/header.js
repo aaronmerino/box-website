4e5fd89afa26ee850442d350b87f716a01c61014
aaron <aaron_merino@outlook.com>
2021-11-11 13:04:15 -0500

    added different pages

---
+++ static-content/scripts/header.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+  let header = document.getElementById("top-header");
+  header.innerHTML = `
+    <div class="container">
+      <h1></b>box</h1>
+      <nav>
+        <ul>
+          <li><a href="/static/index.html">HOME</a></li>
+          <li><a href="/static/drawings.html">DRAWINGS</a></li>
+          <li><a href="/static/about.html">ABOUT</a></li>
+        </ul>
+      </nav>
+    </div>
+  `;
+});
+
+
+
